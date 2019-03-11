ea739971c70d2f4b17ba890f9f4caa912ba01cf5
shaoyuti <yuting.shao@mail.utoronto.ca>
2019-03-10 23:54:51 -0400

    update resource page

--- YouthLine/YouthLine/YouthLine_group/ResourcePage/ResourceCell.swift
+++ YouthLine/YouthLine/YouthLine_group/ResourcePage/ResourceCell.swift
@@ -63,7 +63,7 @@ class ResourceCell: UITableViewCell {
         ResourceImgView.snp.makeConstraints { (make) in
             make.centerY.equalTo(contentView)
             make.right.equalTo(contentView).offset(-10)
-            make.width.equalTo(130)
+            make.width.equalTo(140)
             make.height.equalTo(65)
         }
     }
@@ -84,8 +84,108 @@ class ResourceCell: UITableViewCell {
     }
     
     func setImage(resource: Resource) {
+//        if resource.image.size.width > 140 {
+//            let size = CGSize(width: 140, height: max(65, 140 * resource.image.size.height / resource.image.size.width))
+//            ResourceImgView.image = resource.image.scaledAspectFit(to: size)
+//        } else {
+//            ResourceImgView.image = resource.image
+//        }
         ResourceImgView.image = resource.image
         ResourceTitleLabel.text = resource.title
     }
     
 }
+
+//extension UIImage {
+//
+//    /// Resize the image to be the required size, stretching it as needed.
+//    ///
+//    /// - parameter newSize:      The new size of the image.
+//    /// - parameter contentMode:  The `UIViewContentMode` to be applied when resizing image.
+//    ///                           Either `.scaleToFill`, `.scaleAspectFill`, or `.scaleAspectFit`.
+//    ///
+//    /// - returns:                Return `UIImage` of resized image.
+//
+//    func scaled(to newSize: CGSize, contentMode: UIViewContentMode = .scaleToFill) -> UIImage? {
+//        if contentMode == .scaleToFill {
+//            return filled(to: newSize)
+//        } else if contentMode == .scaleAspectFill || contentMode == .scaleAspectFit {
+//            let horizontalRatio = size.width  / newSize.width
+//            let verticalRatio   = size.height / newSize.height
+//
+//            let ratio: CGFloat!
+//            if contentMode == .scaleAspectFill {
+//                ratio = min(horizontalRatio, verticalRatio)
+//            } else {
+//                ratio = max(horizontalRatio, verticalRatio)
+//            }
+//
+//            let sizeForAspectScale = CGSize(width: size.width / ratio, height: size.height / ratio)
+//            let image = filled(to: sizeForAspectScale)
+//            if contentMode == .scaleAspectFill {
+//                let subRect = CGRect(
+//                    x: floor((sizeForAspectScale.width - newSize.width) / 2.0),
+//                    y: floor((sizeForAspectScale.height - newSize.height) / 2.0),
+//                    width: newSize.width,
+//                    height: newSize.height)
+//                return image?.cropped(to: subRect)
+//            }
+//            return image
+//        }
+//        return nil
+//    }
+//
+//    /// Resize the image to be the required size, stretching it as needed.
+//    ///
+//    /// - parameter newSize:   The new size of the image.
+//    ///
+//    /// - returns:             Resized `UIImage` of resized image.
+//
+//    func filled(to newSize: CGSize) -> UIImage? {
+//        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
+//        draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+//        let image = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//
+//        return image
+//    }
+//
+//    /// Crop the image to be the required size.
+//    ///
+//    /// - parameter bounds:    The bounds to which the new image should be cropped.
+//    ///
+//    /// - returns:             Cropped `UIImage`.
+//
+//    func cropped(to bounds: CGRect) -> UIImage? {
+//        var rect = bounds
+//        rect.size.width *= scale
+//        rect.size.height *= scale
+//
+//        if let imageRef = cgImage?.cropping(to: rect) {
+//            return UIImage(cgImage: imageRef, scale: scale, orientation: imageOrientation)
+//        } else {
+//            return nil
+//        }
+//    }
+//
+//    /// Resize the image to fill the rectange of the specified size, preserving the aspect ratio, trimming if needed.
+//    ///
+//    /// - parameter newSize:   The new size of the image.
+//    ///
+//    /// - returns:             Return `UIImage` of resized image.
+//
+//    func scaledAspectFill(to newSize: CGSize) -> UIImage? {
+//        return scaled(to: newSize, contentMode: .scaleAspectFill);
+//    }
+//
+//    /// Resize the image to fit within the required size, preserving the aspect ratio, with no trimming taking place.
+//    ///
+//    /// - parameter newSize:   The new size of the image.
+//    ///
+//    /// - returns:             Return `UIImage` of resized image.
+//
+//    func scaledAspectFit(to newSize: CGSize) -> UIImage? {
+//        return scaled(to: newSize, contentMode: .scaleAspectFit)
+//    }
+//
+//}
