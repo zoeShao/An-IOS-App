785d329bbadaf27104c6f44b6a92843c6159cc55
linshiq4 <e.lin@mail.utoronto.ca>
2019-04-02 21:00:53 -0400

    lock rotation for the app

--- YouthLine/YouthLine/AppDelegate.swift
+++ YouthLine/YouthLine/AppDelegate.swift
@@ -45,6 +45,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    var enableAllOrientation = false
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if (enableAllOrientation == true){
+            return UIInterfaceOrientationMask.allButUpsideDown
+        }
+        return UIInterfaceOrientationMask.portrait
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
